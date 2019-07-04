d9cd8bb3c04cdf25da4542de0f71e12469cc87f1
Donald F. Coffin <donald.coffin@reminetworks.com>
2019-07-04 15:07:42 -0400

    Adding GET client

---
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,28 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Created by Donald F. Coffin on 07/04/2019 at 14:56
+ */
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById((UUID.randomUUID()));
+
+        assertNotNull(dto);
+    }
+}
